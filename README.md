3e77822aa34b68cd161609c0020b3f872e106f2e
Ali <ali@alikuru.com>
2017-03-20 23:24:20 +0300

    Check whether the settings.conf is available or not
    Also minor edits to README.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Backup-AWS
 A simple bash script for backing up your MySQL databases and virtual hosts' files to AWS S3.
 
-It basically finds every database hosted on your MySQL (or drop-in replacements like MariaDB or Percona) server, create dumps and archives them, then sends those archives with files on your virtual host root to S3. It actually uses `sync` functionality of AWS CLI, so it mirrors your web root and database archives to S3, more like `rsync` than blind FTP transfer. Especially if you choose to use `--delete` parameter while synchronizing, which can be set through the settings file. Check [settings.conf.sample](settings.conf.sample) for the parameter set I use.
+It basically finds every database hosted on your [MySQL](https://github.com/mysql/mysql-server) (or drop-in replacements like [MariaDB](https://github.com/MariaDB/server) and [Percona](https://github.com/percona/percona-server)) server, create dumps and archives them, then sends those archives with files on your virtual hosts root to S3. Since it uses `sync` functionality of AWS CLI, it actually mirrors your web root and database archives to S3, more like `rsync` than a blind FTP upload. Especially if you choose to use `--delete` parameter while synchronizing, which can be set through the settings file. Check [settings.conf.sample](settings.conf.sample) for the set of parameters I use for my transfers.
 
 Though it can be triggered manually, the script is designed to run as a daily cron job. If you don't want a particular database to be dumped synchronized every day, put an underscrore in the beginning of its name and it will be excluded. You can also exclude directories from your webroot. Again, please check [settings.conf.sample](settings.conf.sample) for details.
 
--- backup-aws.sh
+++ backup-aws.sh
@@ -15,66 +15,75 @@ logfile="backup-$(hostname)-$suffix.log"
 exitcodes="exit-codes.log"
 
 # Import settings from config file
-source $scriptpath/settings.conf
+if [[ -f $scriptpath/settings.conf ]]; then
 
-# If it doesn't exist, create the directory for storing database dumps as defined in settings.
-if [[ ! -d "$mysql_output" ]]; then
-  mkdir -p $mysql_output
-fi
+  source $scriptpath/settings.conf
 
-# Dump all databeses and create arcives.
-# Skip databases with names starting with an underscrore. Also, prefer not to dump "information_schema".
-databases=`mysql --user=$mysql_user --password=$mysql_password -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
-echo $? >> $scriptpath/$exitcodes
-for db in $databases; do
-  if [[ "$db" != "information_schema" ]] && [[ "$db" != _* ]] ; then
-    echo -e "========================================\nDumping database: $db\n========================================" >> $scriptpath/$logfile
-    mysqldump --force --opt --add-drop-table --log-error=$scriptpath/$logfile --user=$mysql_user --password=$mysql_password --databases $db > $mysql_output/$db.$suffix.sql
-    echo $? >> $scriptpath/$exitcodes
-    tar cfv $mysql_output/$db.$suffix.sql.tar -C $mysql_output $db.$suffix.sql
-    echo $? >> $scriptpath/$exitcodes
-    7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on $mysql_output/$db.$suffix.tar.7z $mysql_output/$db.$suffix.sql.tar >>$scriptpath/$logfile 2>&1
-    echo $? >> $scriptpath/$exitcodes
-    rm $mysql_output/$db.$rotate.tar.7z
+  # If it doesn't exist, create the directory for storing database dumps as defined in settings.
+  if [[ ! -d "$mysql_output" ]]; then
+    mkdir -p $mysql_output
   fi
-done
-rm $mysql_output/*.sql $mysql_output/*.tar
 
-# Sync all local assets with remote.
-echo -e "========================================\nSynchronizing databases\n========================================" >> $scriptpath/$logfile
-if [[ -z ${s3_sync_exclude+x} ]]; then
-  aws s3 sync $mysql_output s3://$s3_bucket_dbs $s3_sync_params >> $scriptpath/$logfile
+  # Dump all databeses and create arcives.
+  # Skip databases with names starting with an underscrore. Also, prefer not to dump "information_schema".
+  databases=`mysql --user=$mysql_user --password=$mysql_password -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
+  echo $? >> $scriptpath/$exitcodes
+  for db in $databases; do
+    if [[ "$db" != "information_schema" ]] && [[ "$db" != _* ]] ; then
+      echo -e "========================================\nDumping database: $db\n========================================" >> $scriptpath/$logfile
+      mysqldump --force --opt --add-drop-table --log-error=$scriptpath/$logfile --user=$mysql_user --password=$mysql_password --databases $db > $mysql_output/$db.$suffix.sql
+      echo $? >> $scriptpath/$exitcodes
+      tar cfv $mysql_output/$db.$suffix.sql.tar -C $mysql_output $db.$suffix.sql
+      echo $? >> $scriptpath/$exitcodes
+      7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on $mysql_output/$db.$suffix.tar.7z $mysql_output/$db.$suffix.sql.tar >>$scriptpath/$logfile 2>&1
+      echo $? >> $scriptpath/$exitcodes
+      rm $mysql_output/$db.$rotate.tar.7z
+    fi
+  done
+  rm $mysql_output/*.sql $mysql_output/*.tar
+
+  # Sync all local assets with remote.
+  echo -e "========================================\nSynchronizing databases\n========================================" >> $scriptpath/$logfile
+  if [[ -z ${s3_sync_exclude+x} ]]; then
+    aws s3 sync $mysql_output s3://$s3_bucket_dbs $s3_sync_params >> $scriptpath/$logfile
+  else
+    aws s3 sync $mysql_output s3://$s3_bucket_dbs $s3_sync_params --exclude "$s3_sync_exclude" >> $scriptpath/$logfile
+  fi
+  echo $? >> $scriptpath/$exitcodes
+  echo -e "========================================\nSynchronizing virtual hosts\n========================================" >> $scriptpath/$logfile
+  if [[ -z ${s3_sync_exclude+x} ]]; then
+    aws s3 sync $root_vhosts s3://$s3_bucket_vhosts $s3_sync_params >> $scriptpath/$logfile
+  else
+    aws s3 sync $root_vhosts s3://$s3_bucket_vhosts $s3_sync_params --exclude "$s3_sync_exclude" >> $scriptpath/$logfile
+  fi
+  echo $? >> $scriptpath/$exitcodes
+
+  # Check if any errors happened during creating and synchronizing backups.
+  errorcount="$(grep -Ev '(^0|^$)' $scriptpath/$exitcodes|wc -l)"
+
+  # Send the report.
+  report=$(openssl enc -base64 -A -in $scriptpath/$logfile)
+  if [[ $errorcount -eq 0 ]]; then
+    errorstatus="without any errors"
+  elif [[ $errorcount -eq 1 ]]; then
+    errorstatus="with $errorcount error"
+  else
+    errorstatus="with $errorcount errors"
+  fi
+
+  echo "{\"Data\": \"X-SES-SOURCE-ARN: $mail_header_source_arn\nX-SES-FROM-ARN: $mail_header_from_arn\nX-SES-RETURN-PATH-ARN: $mail_header_return_arn\nFrom: $mail_from\nTo: $mail_to\nReturn-Path: $mail_return\nSubject: Backup completed $errorstatus @ $(date) for $(hostname)\nMIME-Version: 1.0\nContent-type: Multipart/Mixed; boundary="NextPart"\n\n--NextPart\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 7bit\n\nPlease find job report attached to this email.\n\n--NextPart\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: base64\nContent-Disposition: attachment; filename="$logfile"\n\n$report\n\n--NextPart--\"}" > $scriptpath/mail.json
+
+  aws ses send-raw-email --raw-message file://$scriptpath/mail.json
+
+  # Set trap for cleanup
+  function cleanup {
+    rm $scriptpath/$logfile $scriptpath/$exitcodes $scriptpath/mail.json
+  }
+  trap cleanup EXIT
+
 else
-  aws s3 sync $mysql_output s3://$s3_bucket_dbs $s3_sync_params --exclude "$s3_sync_exclude" >> $scriptpath/$logfile
+
+  echo "Missing settings.conf file, please refer to README." > error-$(hostname)-$suffix.log
+  exit 0
+
 fi
-echo $? >> $scriptpath/$exitcodes
-echo -e "========================================\nSynchronizing virtual hosts\n========================================" >> $scriptpath/$logfile
-if [[ -z ${s3_sync_exclude+x} ]]; then
-  aws s3 sync $root_vhosts s3://$s3_bucket_vhosts $s3_sync_params >> $scriptpath/$logfile
-else
-  aws s3 sync $root_vhosts s3://$s3_bucket_vhosts $s3_sync_params --exclude "$s3_sync_exclude" >> $scriptpath/$logfile
-fi
-echo $? >> $scriptpath/$exitcodes
-
-# Check if any errors happened during creating and synchronizing backups.
-errorcount="$(grep -Ev '(^0|^$)' $scriptpath/$exitcodes|wc -l)"
-
-# Send the report.
-report=$(openssl enc -base64 -A -in $scriptpath/$logfile)
-if [[ $errorcount -eq 0 ]]; then
-  errorstatus="without any errors"
-elif [[ $errorcount -eq 1 ]]; then
-  errorstatus="with $errorcount error"
-else
-  errorstatus="with $errorcount errors"
-fi
-
-echo "{\"Data\": \"X-SES-SOURCE-ARN: $mail_header_source_arn\nX-SES-FROM-ARN: $mail_header_from_arn\nX-SES-RETURN-PATH-ARN: $mail_header_return_arn\nFrom: $mail_from\nTo: $mail_to\nReturn-Path: $mail_return\nSubject: Backup completed $errorstatus @ $(date) for $(hostname)\nMIME-Version: 1.0\nContent-type: Multipart/Mixed; boundary="NextPart"\n\n--NextPart\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 7bit\n\nPlease find job report attached to this email.\n\n--NextPart\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: base64\nContent-Disposition: attachment; filename="$logfile"\n\n$report\n\n--NextPart--\"}" > $scriptpath/mail.json
-
-aws ses send-raw-email --raw-message file://$scriptpath/mail.json
-
-# Set trap for cleanup
-function cleanup {
-  rm $scriptpath/$logfile $scriptpath/$exitcodes $scriptpath/mail.json
-}
-trap cleanup EXIT
